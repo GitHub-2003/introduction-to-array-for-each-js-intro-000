f61ece1592b1332e1f50dc83faec4473e3a36809
MJU <marie.ju2012@outlook.com>
2020-03-23 01:17:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ function doToElementsInArray(array, changeCompletely) {
 // changeCompletely() alters every element in an array when used in conjunction with
 // Array.prototype.forEach()
 function changeCompletely(element, index, array) {
-  console.log `Mmmm, ${element}!!!`
+  console.log(`Mmmm, ${element}!!!`)
 }
